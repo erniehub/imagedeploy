cfa8fcaa6af8a17b1067ccbe180dc2f39921225c
Thong Kuah <tkuah@gitlab.com>
2019-07-05 13:28:17 +1200

    create_secret and test happy path

--- src/deploy-helpers.sh
+++ src/deploy-helpers.sh
@@ -56,3 +56,19 @@ function initialize_tiller() {
   fi
   echo ""
 }
+
+function create_secret() {
+  echo "Create secret..."
+  if [[ "$CI_PROJECT_VISIBILITY" == "public" ]]; then
+    return
+  fi
+  echo "Creating secret..."
+
+  kubectl create secret -n "$KUBE_NAMESPACE" \
+    docker-registry gitlab-registry \
+    --docker-server="$CI_REGISTRY" \
+    --docker-username="${CI_DEPLOY_USER:-$CI_REGISTRY_USER}" \
+    --docker-password="${CI_DEPLOY_PASSWORD:-$CI_REGISTRY_PASSWORD}" \
+    --docker-email="$GITLAB_USER_EMAIL" \
+    -o yaml --dry-run | kubectl replace -n "$KUBE_NAMESPACE" --force -f -
+}

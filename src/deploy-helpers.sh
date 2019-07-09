5201b1b66eaa186b36255278687c5819f726be9d
Thong Kuah <tkuah@gitlab.com>
2019-07-09 17:11:08 +1200

    download_chart binary

--- src/deploy-helpers.sh
+++ src/deploy-helpers.sh
@@ -6,30 +6,6 @@ auto_database_url=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${CI_ENVIRONM
 export DATABASE_URL=${DATABASE_URL-$auto_database_url}
 export TILLER_NAMESPACE=$KUBE_NAMESPACE
 
-function download_chart() {
-  if [[ ! -d chart ]]; then
-    auto_chart=${AUTO_DEVOPS_CHART:-gitlab/auto-deploy-app}
-    auto_chart_name=$(basename $auto_chart)
-    auto_chart_name=${auto_chart_name%.tgz}
-    auto_chart_name=${auto_chart_name%.tar.gz}
-  else
-    auto_chart="chart"
-    auto_chart_name="chart"
-  fi
-
-  helm init --client-only
-  helm repo add ${AUTO_DEVOPS_CHART_REPOSITORY_NAME:-gitlab} ${AUTO_DEVOPS_CHART_REPOSITORY:-https://charts.gitlab.io} ${AUTO_DEVOPS_CHART_REPOSITORY_USERNAME:+"--username" "$AUTO_DEVOPS_CHART_REPOSITORY_USERNAME"} ${AUTO_DEVOPS_CHART_REPOSITORY_PASSWORD:+"--password" "$AUTO_DEVOPS_CHART_REPOSITORY_PASSWORD"}
-  if [[ ! -d "$auto_chart" ]]; then
-    helm fetch ${auto_chart} --untar
-  fi
-  if [ "$auto_chart_name" != "chart" ]; then
-    mv ${auto_chart_name} chart
-  fi
-
-  helm dependency update chart/
-  helm dependency build chart/
-}
-
 function ensure_namespace() {
   kubectl get namespace "$KUBE_NAMESPACE" || kubectl create namespace "$KUBE_NAMESPACE"
 }

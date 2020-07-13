e1144e93ec64d68ffc3c6228b3f409f2614533bc
Shinya Maeda <shinya@gitlab.com>
2020-07-13 14:38:50 +0700

    feat: Use bundled chart
    BREAKING CHANGE: This is not technically a breaking change as AutoDeploy
    pipelines still use the v0 chart, however, we decided to increment a major
    version for the safety.
    
    Apply 2 suggestion(s) to 2 file(s)

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,15 @@ FROM "registry.gitlab.com/gitlab-org/cluster-integration/helm-install-image/rele
 # https://github.com/sgerrand/alpine-pkg-glibc
 ARG GLIBC_VERSION
 
-COPY src/ build/
-
 # Install Dependencies
 RUN apk add --no-cache openssl curl tar gzip bash jq \
   && curl -sSL -o /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub \
   && curl -sSL -O https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk \
   && apk add glibc-${GLIBC_VERSION}.apk \
-  && apk add ruby jq \
+  && apk add ruby jq ruby-json \
   && rm glibc-${GLIBC_VERSION}.apk
+
+COPY src/ build/
+COPY assets/ assets/
 
 RUN ln -s /build/bin/* /usr/local/bin/
---
+++ src/bin/helpers/gitlab/chart.rb
@@ -0,0 +1,71 @@
+require 'yaml'
+require 'json'
+
+module Gitlab
+  # This class represents auto-deploy-app chart managed by GitLab.
+  class Chart
+    attr_reader :major, :minor, :patch
+
+    GITLAB_MANAGED_CHART_NAME = 'auto-deploy-app'.freeze
+
+    class << self
+      # Load a chart from `helm list` output
+      #
+      # @param [String] data                    JSON formatted `helm list` output
+      # @param [String] release_name            The release created by a chart in question
+      def load_from_helm_ls(data, release_name)
+        # In Helm 2, `helm ls --output json` returns an empty string when there are no releases
+        return if data.empty?
+
+        release = JSON.parse(data)['Releases'].find { |r| r['Name'] == release_name }
+
+        return if release.nil?
+
+        name, major, minor, patch = release['Chart'].scan(/\A(.+)-(\d+)\.(\d+)\.(\d+)/).first
+
+        return unless gitlab_managed_chart?(name, major, minor, patch)
+
+        self.new(major, minor, patch)
+      end
+
+      # Load a chart from Chart.yaml
+      #
+      # @param [String] chart_dir                The path to the chart directory
+      def load_from_chart_yml(chart_dir)
+        chart_hash = YAML.load_file(File.join(chart_dir, 'Chart.yaml'))
+
+        name = chart_hash['name']
+        major, minor, patch = chart_hash['version'].scan(/\A(\d+)\.(\d+)\.(\d)+/).first
+
+        return unless gitlab_managed_chart?(name, major, minor, patch)
+
+        self.new(major, minor, patch)
+      end
+
+      def gitlab_managed_chart?(name, major, minor, patch)
+        name == GITLAB_MANAGED_CHART_NAME && major && minor && patch
+      end
+    end
+
+    def initialize(major, minor, patch)
+      @major = major.to_i
+      @minor = minor.to_i
+      @patch = patch.to_i
+    end
+
+    def to_s
+      "v#{major}.#{minor}.#{patch}"
+    end
+
+    def compatible?(previous_chart)
+      # v0 and v1 charts are compatible
+      return true if major == 1 && previous_chart.major == 0
+
+      major == previous_chart.major
+    end
+
+    def allowed_to_force_deploy?
+      ENV["AUTO_DEVOPS_FORCE_DEPLOY_V#{major}"]
+    end
+  end
+end

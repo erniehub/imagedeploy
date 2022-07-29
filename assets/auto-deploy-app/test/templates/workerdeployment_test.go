1da71d895ad8dcbb55f69571329ebc0e8f0c899f
Carlos Leandro <carlos.leandro@parafernalia.net.br>
2022-07-29 16:35:33 -0300

    fix: pointers

--- assets/auto-deploy-app/test/templates/workerdeployment_test.go
+++ assets/auto-deploy-app/test/templates/workerdeployment_test.go
@@ -786,12 +786,12 @@ func TestWorkerDeploymentTemplate(t *testing.T) {
 				{
 					ExpectedName:           "production-worker1",
 					ExpectedCmd:            []string{"echo", "worker1"},
-					ExpectedResources:  	&coreV1.ResourceRequirements{},
+					ExpectedResources:  	coreV1.ResourceRequirements{},
 				},
 				{
 					ExpectedName:           "production-worker2",
 					ExpectedCmd:            []string{"echo", "worker2"},
-					ExpectedResources:  	&coreV1.ResourceRequirements{},
+					ExpectedResources:  	coreV1.ResourceRequirements{},
 				},
 			},
 		},
@@ -809,7 +809,7 @@ func TestWorkerDeploymentTemplate(t *testing.T) {
 				{
 					ExpectedName:           "production-worker1",
 					ExpectedCmd:            []string{"echo", "worker1"},
-					ExpectedResources:  	&coreV1.ResourceRequirements{
+					ExpectedResources:  	coreV1.ResourceRequirements{
 						Requests: coreV1.ResourceList{
 							"memory": resource.MustParse("250M"),
 						},
@@ -818,7 +818,7 @@ func TestWorkerDeploymentTemplate(t *testing.T) {
 				{
 					ExpectedName:           "production-worker2",
 					ExpectedCmd:            []string{"echo", "worker2"},
-					ExpectedResources:  	&coreV1.ResourceRequirements{},
+					ExpectedResources:  	coreV1.ResourceRequirements{},
 				},
 			},
 		},
@@ -850,7 +850,7 @@ func TestWorkerDeploymentTemplate(t *testing.T) {
 				require.Equal(t, expectedDeployment.ExpectedName, deployment.Name)
 				require.Len(t, deployment.Spec.Template.Spec.Containers, 1)
 				require.Equal(t, expectedDeployment.ExpectedCmd, deployment.Spec.Template.Spec.Containers[0].Command)
-				require.Equal(t, expectedDeployment.ExpectedResources, &deployment.Spec.Template.Spec.Containers[0].Resources)
+				require.Equal(t, expectedDeployment.ExpectedResources, deployment.Spec.Template.Spec.Containers[0].Resources)
 			}
 		})
 	}

859e4a7c992f0e993eb21feb817d11e847b96a63
Alexander Rusa <alex@rusa.at>
2024-01-23 12:28:24 +0000

    feat: add extraEnvFrom to db-migrate and db-initialize

--- assets/auto-deploy-app/test/templates/db_initialize_job_test.go
+++ assets/auto-deploy-app/test/templates/db_initialize_job_test.go
@@ -231,3 +231,48 @@ func TestInitializeDatabaseLabels(t *testing.T) {
 		})
 	}
 }
+
+func TestInitializeDatabaseTemplateWithExtraEnvFrom(t *testing.T) {
+	releaseName := "initialize-application-database-extra-envfrom"
+	templates := []string{"templates/db-initialize-job.yaml"}
+
+	tcs := []struct {
+		name            string
+		values          map[string]string
+		expectedEnvFrom coreV1.EnvFromSource
+	}{
+		{
+			name: "with extra envfrom secret test",
+			values: map[string]string{
+				"application.initializeCommand": "echo initialize",
+				"extraEnvFrom[0].secretRef.name": "secret-name-test",
+			},
+			expectedEnvFrom: coreV1.EnvFromSource{
+				SecretRef: &coreV1.SecretEnvSource{
+					LocalObjectReference: coreV1.LocalObjectReference{
+						Name: "secret-name-test",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			namespaceName := "minimal-ruby-app-" + strings.ToLower(random.UniqueId())
+
+			options := &helm.Options{
+				SetValues: tc.values,
+				KubectlOptions: k8s.NewKubectlOptions("", "", namespaceName),
+			}
+
+			output := mustRenderTemplate(t, options, releaseName, templates, nil)
+
+			var deployments deploymentAppsV1List
+			helm.UnmarshalK8SYaml(t, output, &deployments)
+			for _, deployment := range deployments.Items {
+				require.Contains(t, deployment.Spec.Template.Spec.Containers[0].EnvFrom, tc.expectedEnvFrom)
+			}
+		})
+	}
+}
